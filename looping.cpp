02acb44367adb67a8e6d46d045eec5993207cdd6
sykllh <sykllhidmi@gmail.com>
2026-03-30 15:55:39 +0700

    Tambah perulangan dengan FOR

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main(){
+    int x;
+
+    //perulangan dengan FOR
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+}
