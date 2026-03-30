0254fdaf0d7083a72e0c29d6ae565d88427f8adf
sykllh <sykllhidmi@gmail.com>
2026-03-30 17:14:43 +0700

    untuk kelulusan nya

--- looping.cpp
+++ looping.cpp
@@ -64,3 +64,45 @@ logika OR
 #include <iostream>
 using namespace std;
 
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+        cout<<"Status Kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
+
+logika AND
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin<<nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin<<nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status Kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
+
