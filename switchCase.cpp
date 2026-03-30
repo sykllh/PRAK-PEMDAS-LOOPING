d789a71f5a0afd7d7cc6749cf4ef0064cfe1d4ce
sykllh <sykllhidmi@gmail.com>
2026-03-30 17:34:26 +0700

    prosedur input

--- switchCase.cpp
+++ switchCase.cpp
@@ -6,4 +6,18 @@ int kodeJurusan;
 string namaJurusan;
 string statusKouta;
 
-//
+//prosedur input
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;   
+}
+
+//prosedur untuk pengecekan kondisi
+void pilihanJurusan() {
+    
+}
