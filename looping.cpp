f4906dd973913d54f884d7654f7f35568dc0f290
sykllh <sykllhidmi@gmail.com>
2026-03-30 16:13:07 +0700

    tambah input nilai

--- looping.cpp
+++ looping.cpp
@@ -42,3 +42,13 @@ int main(){
 logika not
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilB.nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+}
